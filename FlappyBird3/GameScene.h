06aad203d89a3994c71022ceda174d4afe8cca36
sys <1052856576@qq.com>
2015-12-23 23:23:15 +0800

    Initial Commit

---
+++ FlappyBird3/GameScene.h
@@ -0,0 +1,13 @@
+//
+//  GameScene.h
+//  FlappyBird3
+//
+
+//  Copyright (c) 2015年 sys. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameScene : SKScene
+
+@end
---
+++ FlappyBird3/GameViewController.h
@@ -0,0 +1,14 @@
+//
+//  GameViewController.h
+//  FlappyBird3
+//
+
+//  Copyright (c) 2015年 sys. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameViewController : UIViewController
+
+@end
